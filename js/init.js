a1f526b8fe59fb52f732ad3099ab031d729dc23a
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-06 14:18:12 +0200

    added setup function

--- js/init.js
+++ js/init.js
@@ -1,5 +1,13 @@
 //Immediately-invoked function expression
 //Runs code immediately when the page loads and keeps it out of the global scope (avoids naming conflicts)
 (function () {
-    
+    setup();
 })(); 
+
+function setup(){
+    const wh = 600;
+    const canvas = document.createElement('canvas'); 
+    canvas.setAttribute('width', wh);
+    canvas.setAttribute('height', wh); 
+    document.body.insertBefore(canvas, document.body.childNodes[0]); 
+}
