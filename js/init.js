895dc8cee4c50660fb3be05c2d0c2631bcda63d4
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-08 11:27:14 +0200

    progress

--- js/init.js
+++ js/init.js
@@ -5,13 +5,14 @@ const wh = 600;
 const cellSize = 30; 
 const rows = 20;
 const cols = 20;
-const grid = new Array(cols); 
+const grid = new Array(rows); 
 
 //FUNCTIONS 
 //Immediately-invoked function expression
 //Runs code immediately when the page loads and keeps it out of the global scope (avoids naming conflicts)
 (function () {
     setup();
+    createGrid();
     // drawGrid();
 })(); 
 
@@ -21,6 +22,10 @@ function Cell() { //Constructor function for each cell in the array
     this.f = 0;
     this.g = 0;
     this.h = 0;
+
+    this.show = function(){ //function to show cell on grid 
+        ctx.strokeRect(this.x, this.y, cellSize, cellSize);
+    }
 }
 
 //Function to setup the canvas 
@@ -28,37 +33,25 @@ function setup(){
     canvas.setAttribute('width', wh);
     canvas.setAttribute('height', wh); 
     document.body.insertBefore(canvas, document.body.childNodes[0]); //Inserts canvas before the first element in body 
+}
 
-    //Creating Grid
-    for (let i = 0; i < cols; i++){ //Creating 2D array
-        grid[i] = new Array(rows);
+//Function to create grid 
+function createGrid(){
+    for (let i = 0; i < rows; i++){ //Creating 2D array
+        grid[i] = new Array(cols);
     }
     let x = 0;
     let y = 0;
-    for (let i = 0; i < cols; i++){ //Creating a new cell for each spot in the array
-        for (let j = 0; j < rows; j++){
+    for (let i = 0; i < rows; i++){ //Creating a new cell for each spot in the array
+        for (let j = 0; j < cols; j++){
             grid[i][j] = new Cell();
             x = x + 1 * 30;
             grid[i][j].x = x;
+            grid[i][j].y = y;
+            grid[i][j].show();
         }
         x = 0;
+        y = y + 1 * 30;
     }
     console.log(grid);
 }
-
-// function draw(x, y){
-//     ctx.strokeRect(x, y, cellSize, cellSize);
-// }
-
-// function drawGrid(){
-//     let x = 0; 
-//     let y = 0;
-//     for (let j = 0; j < rows; j++){
-//         for (let i = 0; i < cols; i++){
-//             draw(x, y, cellSize, cellSize);
-//             x = x + 1 * 30;
-//         }
-//         x = 0;   
-//         y = y + 1 * 30;  
-//     }
-// }
