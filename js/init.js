8a044bb323130520feffdd958fec41fdccc3bdd2
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-09 15:59:18 +0200

    progress

--- js/init.js
+++ js/init.js
@@ -1,92 +1,100 @@
 //GLOBAL VARIABLES 
-const canvas = document.createElement('canvas'); 
+const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 const wh = 600;
-const cellSize = 30; 
+const cellSize = 30;
 const rows = 20;
 const cols = 20;
-const grid = new Array(rows); 
-const open = [];
+const grid = new Array(rows);
+let open = [];
 const closed = [];
 let start;
-let end; 
+let end;
 
 //FUNCTIONS 
 //Immediately-invoked function expression
 //Runs code immediately when the page loads and keeps it out of the global scope (avoids naming conflicts)
-(function () {
-    setup();
-})(); 
+(function() {
+  setup();
+})();
 
 function Cell(x, y) { //Constructor function for each cell in the array
-    this.x = 0;
-    this.y = 0;
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-    this.show = function(color){ //Function to show cell on grid 
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, cellSize, cellSize);
-        ctx.strokeStyle = 'white';
-        ctx.strokeRect(this.x, this.y, cellSize, cellSize);
-    }
+  this.x = x;
+  this.y = y;
+  this.show = function(color) { //Function to show cell on grid 
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x * cellSize, this.y * cellSize, cellSize, cellSize);
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(this.x * cellSize, this.y * cellSize, cellSize, cellSize);
+  }
 }
 
 //Function to setup the canvas 
-function setup(){
-    let interval = setInterval(update, 120);
-    canvas.setAttribute('width', wh);
-    canvas.setAttribute('height', wh); 
-    document.body.insertBefore(canvas, document.body.childNodes[0]); //Inserts canvas before the first element in body 
-    createGrid();
-    setStartEnd();
+function setup() {
+  let interval = setInterval(update, 120);
+  canvas.setAttribute('width', wh);
+  canvas.setAttribute('height', wh);
+  document.body.insertBefore(canvas, document.body.childNodes[0]); //Inserts canvas before the first element in body 
+  createGrid();
+  setStartEnd();
 }
 
 //Function to create grid 
-function createGrid(){
-    for (let i = 0; i < rows; i++){ //Creating 2D array
-        grid[i] = new Array(cols);
+function createGrid() {
+  for (let i = 0; i < rows; i++) { //Creating 2D array
+    grid[i] = new Array(cols);
+  }
+  for (let i = 0; i < rows; i++) { //Creating a new cell for each spot in the array
+    for (let j = 0; j < cols; j++) {
+      grid[i][j] = new Cell(i, j);
+      grid[i][j].show();
     }
-    let x = 0;
-    let y = 0;
-    for (let i = 0; i < rows; i++){ //Creating a new cell for each spot in the array
-        for (let j = 0; j < cols; j++){
-            grid[i][j] = new Cell();
-            grid[i][j].x = x;
-            grid[i][j].y = y;
-            grid[i][j].show();
-            x = x + 1 * 30;
-        }
-        x = 0;
-        y = y + 1 * 30;
-    }
+  }
 }
 
 //Function that defines the start and end points
-function setStartEnd(){
-    start = grid[0][0];
-    end = grid[cols-1][rows-1];
-    open.push(start);
+function setStartEnd() {
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+  open.push(start);
 }
 
 //Function to remove a node from an array
-function removeArray(arr, e){
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] === e){
-            arr.splice(i, 1);
-        }
+function removeArray(arr, e) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === e) {
+      arr.splice(i, 1);
     }
+  }
 }
 
 //Main function
-function update(){
-    //nodes part of "open" array are green 
-    for (let i = 0; i < open.length; i++){
-        open[i].show('green');
-    }
+function update() {
+  //nodes part of "open" array are green 
+  for (let i = 0; i < open.length; i++) {
+    open[i].show('green');
+  }
 
-    //nodes part of "closed" array are red
-    for (let i = 0; i < closed.length; i++){
-        closed[i].show('red');
-    }
+  //nodes part of "closed" array are red
+  for (let i = 0; i < closed.length; i++) {
+    closed[i].show('red');
+  }
+  
+  const nextOpenSet = new Set();
+  open.forEach(cell => {
+    const above = grid[cell.y - 1]?.[cell.x];
+    if (above) nextOpenSet.add(above);
+    
+    const below = grid[cell.y + 1]?.[cell.x];
+    if (below) nextOpenSet.add(below);
+    
+    const left = grid[cell.y][cell.x - 1];
+    if (left) nextOpenSet.add(left);
+    
+    const right = grid[cell.y][cell.x + 1];
+    if (right) nextOpenSet.add(right);
+  });
+  
+  open = Array.from(nextOpenSet);
+  
 }
